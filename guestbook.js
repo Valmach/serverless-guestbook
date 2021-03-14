b55a4ee27e59d09387a1406e03460ce50bfec905
valmach <shouraigen@gmail.com>
2021-03-13 20:21:58 -0600

    add apiuri

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://96962e74.us-south.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://2e0a3a17.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
